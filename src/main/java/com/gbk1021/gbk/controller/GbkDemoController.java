aad9a170ef67e8ed72fe5584a4bc2c2760f97bd3
moong94 <hmg19940201@naver.com>
2024-03-31 01:05:44 +0900

    React - SpringBoot 연동

---
+++ src/main/java/com/gbk1021/gbk/controller/GbkDemoController.java
@@ -0,0 +1,14 @@
+package com.gbk1021.gbk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GbkDemoController {
+    
+    @RequestMapping("/api/test")
+    public String demo() {
+        return "GoodBye-Korea 프로젝트 시작1";
+    }
+}
