e893b57656c00a77e2fb7d82d6f0722409db4b0d
moong94 <hmg19940201@naver.com>
2024-05-15 20:26:16 +0900

    main page publising

--- src/main/gbk-frontend/src/components/gbkMain.js
+++ src/main/gbk-frontend/src/components/gbkMain.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { GbkFooter, GbkHeader } from './gbkCommon'
 import './gbkMain.css'
+import { Link } from 'react-router-dom';
 
 // functional Component
 function GbkMain() {
@@ -9,11 +10,24 @@ function GbkMain() {
     <div>
       <GbkHeader />
       <div id="container">
-        <div id="gbkMainAnimationTab">
-          애니메이션 탭 컨텐츠
+        <div id="gbkMainAnimationContainer">
+          <div id="gbkMainAnimationTab">
+            <div id="gbkMainDecrease">인구감소<img class="decrease_image" src={require("../images/decrease.png")} alt="decreasing icon"></img></div>
+            <div id="gbkMainOld">초고령사회 진입<img class="oldman_image" src={require("../images/old_man.png")} alt="old man icon"></img></div>
+            <div id="gbkMainAnimation">
+              51,751,065 명
+            </div>
+          </div>
         </div>
+        <div class="contentsDivider"></div>
         <div id="gbkMainBoard">
-          게시판 컨텐츠
+          <div id="gbkMainBoardTitle">
+            <div id="boardTitleBox"><Link id="boardTitle">게시판</Link></div>
+            <div id="boardMoreBox"><Link id="boardMore">더보기 <i class="fa-solid fa-angle-right"></i></Link></div>
+          </div>
+          <div>
+            <img class="Joan-Wiliams" src={require("../images/williams.jpg")} alt="Joan Williams said 'Korea is so screwed. Wow!'. You can link to board page."></img>
+          </div>
         </div>
       </div>
 {/*       <div className="gbk-animation-tab">
