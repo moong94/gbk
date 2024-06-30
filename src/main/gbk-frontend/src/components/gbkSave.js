9be9576f9344042b1a4aa589bf9ef36ed9e2aa9b
moong94 <hmg19940201@naver.com>
2024-06-30 17:57:34 +0900

    대한민국 구하기 페이지 개발

--- src/main/gbk-frontend/src/components/gbkSave.js
+++ src/main/gbk-frontend/src/components/gbkSave.js
@@ -1,27 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { GbkFooter, GbkHeader } from './gbkCommon'
 import './gbkSave.css'
 
+
+
 // functional Component
 function GbkSave() {
+  const petitionsUrl="https://www.naver.com";
+  const kakaoUrl="https://qr.kakaopay.com/Ej8k7QOHq";
+
+  const [toast, setToast] = useState(false);
+
+  const copyAccpountNum = async (accountNumber) => {
+    await navigator.clipboard.writeText(accountNumber);
+    setToast(true);
+  };
+  
+  useEffect(() => {
+    if (toast) {
+      const timer = setTimeout(() => {
+        setToast(false);
+      }, 3000);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [toast]);
 
   return (
     <div>
       <GbkHeader />
       <div id="container">
-        <div id="gbkMainAnimationTab">
-          대한민국구하기
+        <div id="saveTopContents">
+          <div id="koreanFlagContainer">
+            
+            {/* <img id="koreanFlagImg" src={require("../images/korean_flag.png")} alt="Korean Flag"></img> */}
+            <div id="koreanFlagText">
+              <div className="koreanFlagMainText">멸망해 가는 대한민국을 구해주세요</div>
+              <div className="linkToPetitions"><a id="petitions" href={petitionsUrl} target="_blank" rel="noopener noreferrer">국민청원 참여하기 <i className="fa-solid fa-angle-right"></i></a></div>
+            </div>
+          
+          </div>
         </div>
-        <div id="gbkMainBoard">
-          게시판 컨텐츠
+        <div className="contentsDivider"></div>
+        <div id="saveBottomContents">
+          <div className="saveBottomTitle">후원안내</div>
+          <div className="saveBottomText">후원해주신 금액은 대한민국 인구감소 문제해결에 사용됩니다.</div>
+          <div className="saveBottomAccountInfo">
+            <div className="accountImg">계좌정보</div>
+            <div className="accountNumber" onClick={() => copyAccpountNum("신한은행 110-389-199004")}>신한은행 110-389-199004<img className="copyImage" src={require('../images/copy_image.png')} alt="copy"></img></div>
+            <div className="accountUserName">예금주 : 홍문기</div>
+          </div>
+          <div className="donateToKakao">
+            <div className="kakaoPayImg"></div>
+            <a className="linkToKakao" href={kakaoUrl} target="_blank" rel="noopener noreferrer">카카오페이로 후원하기</a>
+          </div>
         </div>
       </div>
-{/*       <div className="gbk-animation-tab">
-         {this.gbk-animation}
-      </div>
-      <div className="gbk-board">
-        게시판
-      </div> */}
+      {toast && (
+      <div id="copyToastPopup" >
+          계좌번호가 복사됐어요
+        </div>
+      )}
       <GbkFooter />
     </div>
   )
